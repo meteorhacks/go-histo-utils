4647998f15a4cc6b7d6008b27de9d5da1dcb33a8
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2015-05-12 07:48:13 +0530

    Add perBinCalculation logic

--- percentiles.go
+++ percentiles.go
@@ -2,6 +2,7 @@ package histoutils
 
 import (
 	"math"
+	"sort"
 )
 
 type PercentileInfo struct {
@@ -9,17 +10,18 @@ type PercentileInfo struct {
 	i int
 }
 
-func GetPercentiles(h *Histogram, percentiles []int) map[int]float64 {
+func GetPercentiles(h *Histogram, percentiles []int, perbinCalculation bool) map[int]float64 {
 	finalResults := make(map[int]float64)
 	totalItems := 0
 	for _, bin := range h.Bins {
 		totalItems += bin.Y
 	}
 
+	sort.Ints(percentiles)
 	percentileMapper := make([]PercentileInfo, len(percentiles))
-	for _, percentile := range percentiles {
-		percentileMapper[0].p = percentile
-		percentileMapper[0].i = int(math.Ceil(float64(percentile) / 100.0 * float64(totalItems)))
+	for lc, percentile := range percentiles {
+		percentileMapper[lc].p = percentile
+		percentileMapper[lc].i = int(math.Ceil(float64(percentile) / 100.0 * float64(totalItems)))
 	}
 
 	itemsUpto := 0
@@ -33,7 +35,13 @@ func GetPercentiles(h *Histogram, percentiles []int) map[int]float64 {
 			percentileMapper = percentileMapper[1:]
 
 			if percentileInfo.i > beginItems && percentileInfo.i <= endItems {
-				finalResults[percentileInfo.p] = float64(bin.X)
+				percentile := float64(bin.X)
+				if perbinCalculation {
+					diff := float64(percentileInfo.i - beginItems)
+					binsPerItem := float64(h.BinSize) / float64(bin.Y)
+					percentile = float64(bin.X) + (diff * binsPerItem)
+				}
+				finalResults[percentileInfo.p] = percentile
 			} else {
 				percentileMapper = append([]PercentileInfo{percentileInfo}, percentileMapper...)
 				break
--- percentiles_test.go
+++ percentiles_test.go
@@ -10,8 +10,61 @@ func TestSinglePercentile(t *testing.T) {
 	h.AddBin(Bin{10, 20})
 	h.AddBin(Bin{20, 5})
 
-	pecentiles := GetPercentiles(&h, []int{50})
-	if pecentiles[50] != 10 {
+	percentiles := GetPercentiles(&h, []int{50}, false)
+	if percentiles[50] != 10 {
+		t.Error("Wrong percentile")
+	}
+}
+
+func TestMultiplePercentiles(t *testing.T) {
+	h := Histogram{BinSize: 10}
+	h.AddBin(Bin{0, 5})
+	h.AddBin(Bin{10, 20})
+	h.AddBin(Bin{20, 5})
+
+	percentiles := GetPercentiles(&h, []int{50, 95}, false)
+	if percentiles[50] != 10 {
+		t.Error("Wrong percentile")
+	}
+
+	if percentiles[95] != 20 {
+		t.Error("Wrong percentile")
+	}
+}
+
+func TestUnorderedMultiplePercentiles(t *testing.T) {
+	h := Histogram{BinSize: 10}
+	h.AddBin(Bin{0, 5})
+	h.AddBin(Bin{10, 20})
+	h.AddBin(Bin{20, 5})
+
+	percentiles := GetPercentiles(&h, []int{95, 50}, false)
+	if percentiles[50] != 10 {
+		t.Error("Wrong percentile")
+	}
+
+	if percentiles[95] != 20 {
+		t.Error("Wrong percentile")
+	}
+}
+
+func TestMultiplePercentilesWithPerbin(t *testing.T) {
+	h := Histogram{BinSize: 100}
+	h.AddBin(Bin{100, 10})
+	h.AddBin(Bin{200, 80})
+	h.AddBin(Bin{300, 8})
+	h.AddBin(Bin{400, 2})
+
+	percentiles := GetPercentiles(&h, []int{50, 95, 99}, true)
+	if percentiles[50] != 250 {
+		t.Error("Wrong percentile")
+	}
+
+	if percentiles[95] != 362.5 {
+		t.Error("Wrong percentile")
+	}
+
+	if percentiles[99] != 450 {
 		t.Error("Wrong percentile")
 	}
 }
